1e5882542fc663ddf24414aaa486916c6d970705
Andrew Drozdov <andrew@mrdrozdov.com>
2021-03-06 18:22:47 -0500

    Uncomment URLs.

--- download_data.sh
+++ download_data.sh
@@ -3,12 +3,11 @@
 mkdir -p data
 cd data
 
-#wget http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar
+wget http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar
 tar xvf images.tar
 
-#wget http://vision.stanford.edu/aditya86/ImageNetDogs/annotation.tar
+wget http://vision.stanford.edu/aditya86/ImageNetDogs/annotation.tar
 tar xvf annotation.tar
 
-#wget http://vision.stanford.edu/aditya86/ImageNetDogs/lists.tar
+wget http://vision.stanford.edu/aditya86/ImageNetDogs/lists.tar
 tar xvf lists.tar
-
