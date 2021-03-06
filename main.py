391630a6d529bdcb188015bc8040bb73722cce16
Andrew Drozdov <andrew@mrdrozdov.com>
2021-03-06 15:27:08 -0500

    Add option to use preset args.

--- main.py
+++ main.py
@@ -19,6 +19,35 @@ from ignite.handlers import ModelCheckpoint, global_step_from_engine
 from ignite.metrics import Metric, RunningAverage
 
 
+default_train_config = {
+    'lr': 0.002,
+    'max_epochs': 5,
+    'log_every_iteration': 10,
+    'save_every_iteration': 10,
+}
+
+
+default_model_config = {
+    'name': 'net',
+}
+
+
+default_train_data_config = {
+    'batch_size': 16,
+    'num_workers': 4,
+    'metadata_path': './data/train_list.mat',
+    'images_folder': './data/Images',
+}
+
+
+default_eval_data_config = {
+    'batch_size': 16,
+    'num_workers': 4,
+    'metadata_path': './data/test_list.mat',
+    'images_folder': './data/Images',
+}
+
+
 class CustomDataset(torch.utils.data.Dataset):
 
     def __init__(self, metadata_path=None, images_folder=None, transform=None, target_transform=None):
@@ -141,25 +170,25 @@ def main(args):
         ])
 
     train_data_config = json.loads(args.train_data_config)
-    test_data_config = json.loads(args.test_data_config)
+    eval_data_config = json.loads(args.eval_data_config)
 
     train_dataset = CustomDataset(
         metadata_path=train_data_config['metadata_path'],
         images_folder=train_data_config['images_folder'],
         transform=transform)
     train_loader = torch.utils.data.DataLoader(train_dataset,
-        batch_size=train_data_config.get('batch_size', 16),
+        batch_size=train_data_config['batch_size'],
         shuffle=True,
-        num_workers=train_data_config.get('num_workers', 4))
+        num_workers=train_data_config['num_workers'])
 
-    test_dataset = CustomDataset(
-        metadata_path=test_data_config['metadata_path'],
-        images_folder=test_data_config['images_folder'],
+    eval_dataset = CustomDataset(
+        metadata_path=eval_data_config['metadata_path'],
+        images_folder=eval_data_config['images_folder'],
         transform=transform)
-    test_loader = torch.utils.data.DataLoader(test_dataset,
-        batch_size=test_data_config.get('batch_size', 16),
+    eval_loader = torch.utils.data.DataLoader(eval_dataset,
+        batch_size=eval_data_config['batch_size'],
         shuffle=False,
-        num_workers=test_data_config.get('num_workers', 4))
+        num_workers=eval_data_config['num_workers'])
 
     train_config = json.loads(args.train_config)
 
@@ -167,7 +196,7 @@ def main(args):
     context['num_classes'] = 120
     context['device'] = device
     model = build_model(args, context).to(device)
-    opt = optim.Adam(model.parameters(), lr=train_config.get('lr', 0.002))
+    opt = optim.Adam(model.parameters(), lr=train_config['lr'])
 
     def train_step(engine, batch):
         model.train()
@@ -191,34 +220,14 @@ def main(args):
 
         return out
 
-    @torch.no_grad()
-    def eval_step(engine, batch):
-        model.eval()
-        images, labels = batch
-        images = images.to(device)
-        labels = labels.to(device)
-        logits = model(images)
-        loss = nn.CrossEntropyLoss()(logits, labels)
-
-        correct = (logits.max(-1)[1] == labels).sum().item()
-        total = labels.shape[-1]
-
-        out = {}
-        out['loss'] = loss.item()
-        out['correct'] = correct
-        out['total'] = total
-
-        return out
-
     trainer = Engine(train_step)
-    evaluator = Engine(eval_step)
 
     @trainer.on(Events.STARTED)
     def start_training(engine):
         print('Training started!')
 
     @trainer.on(Events.EPOCH_COMPLETED)
-    @trainer.on(Events.ITERATION_COMPLETED(every=10))
+    @trainer.on(Events.ITERATION_COMPLETED(every=train_config['log_every_iteration']))
     def log_train(engine):
         log_msg = f"[train] epoch: {engine.state.epoch}"
         log_msg += f" | epoch iteration: {engine.state.metrics['trn_epoch_iteration']} / {engine.state.epoch_length}"
@@ -231,7 +240,7 @@ def main(args):
     def run_eval(engine):
         outputs = []
 
-        for batch in tqdm(test_loader, desc='Eval', disable=not args.progress):
+        for batch in tqdm(eval_loader, desc='Eval', disable=not args.progress):
             images, labels = batch
             images = images.to(device)
             labels = labels.to(device)
@@ -305,16 +314,16 @@ def main(args):
         require_empty=False,
     )
 
-    @trainer.on(Events.ITERATION_COMPLETED(every=train_config.get('save_every_iteration', 20)))
-    def func(engine):
+    @trainer.on(Events.ITERATION_COMPLETED(every=train_config['save_every_iteration']))
+    def save_model_every_iteration(engine):
         epoch_iteration = engine.state.metrics['trn_epoch_iteration']
         epoch_length = engine.state.epoch_length
 
-        log_msg = f"[train] epoch: {engine.state.epoch}"
-        log_msg += f" | epoch iteration: {engine.state.metrics['trn_epoch_iteration']} / {engine.state.epoch_length}"
-        log_msg += f" | total iteration: {engine.state.iteration}"
-        log_msg += f" | saving model"
-        print(log_msg)
+        # log_msg = f"[train] epoch: {engine.state.epoch}"
+        # log_msg += f" | epoch iteration: {engine.state.metrics['trn_epoch_iteration']} / {engine.state.epoch_length}"
+        # log_msg += f" | total iteration: {engine.state.iteration}"
+        # log_msg += f" | saving model"
+        # print(log_msg)
 
         save_iteration_handler(engine, to_save)
 
@@ -330,14 +339,22 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_config', default=json.dumps(dict()), type=str)
-    parser.add_argument('--model_config', default=json.dumps(dict(name='net')), type=str)
-    parser.add_argument('--train_data_config', default=json.dumps(dict(metadata_path='./data/train_list.mat', images_folder='./data/Images')), type=str)
-    parser.add_argument('--test_data_config', default=json.dumps(dict(metadata_path='./data/test_list.mat', images_folder='./data/Images')), type=str)
+    parser.add_argument('--train_config', default=None, type=str)
+    parser.add_argument('--model_config', default=None, type=str)
+    parser.add_argument('--train_data_config', default=None, type=str)
+    parser.add_argument('--eval_data_config', default=None, type=str)
     parser.add_argument('--seed', default=11, type=int)
     parser.add_argument('--cuda', action='store_true', help='If True, run on GPU.')
     parser.add_argument('--progress', action='store_true', help='If True, show progress bar.')
+    parser.add_argument('--preset', default=None, type=str)
     args = parser.parse_args()
+
+    if args.preset is not None:
+        if args.preset == 'default':
+            args.train_config = json.dumps(default_train_config)
+            args.model_config = json.dumps(default_model_config)
+            args.train_data_config = json.dumps(default_train_data_config)
+            args.eval_data_config = json.dumps(default_eval_data_config)
 
     print(args)
 
