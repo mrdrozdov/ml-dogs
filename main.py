528f474c2079a6da32eb2575598be898622a0550
Andrew Drozdov <andrew@mrdrozdov.com>
2021-03-06 11:41:12 -0500

    More accurate accuracy.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import torchvision.transforms as transforms
 from ignite.contrib.handlers import ProgressBar
 from ignite.engine import Engine, Events
 from ignite.handlers import ModelCheckpoint, global_step_from_engine
-from ignite.metrics import RunningAverage
+from ignite.metrics import Metric, RunningAverage
 
 
 class CustomDataset(torch.utils.data.Dataset):
@@ -57,6 +57,34 @@ class CustomDataset(torch.utils.data.Dataset):
         assert len(set(labels)) == max(labels) + 1
 
         return filenames, labels
+
+
+class CustomAccuracyMetric(Metric):
+
+    required_output_keys = ('correct', 'total')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def update(self, output):
+        correct, total = output
+        self._correct += correct
+        self._total += total
+
+    def reset(self):
+        self._correct = 0
+        self._total = 0
+
+    def compute(self):
+        return self._correct / self._total
+
+    def attach(self, engine, name, _usage=None):
+        # restart every epoch
+        engine.add_event_handler(Events.EPOCH_STARTED, self.started)
+        # compute metric
+        engine.add_event_handler(Events.ITERATION_COMPLETED, self.iteration_completed)
+        # apply metric
+        engine.add_event_handler(Events.ITERATION_COMPLETED, self.completed, name)
 
 
 class Net(nn.Module):
@@ -155,7 +183,7 @@ def main(args):
     pbar.attach(trainer)
 
     RunningAverage(output_transform=lambda out: out['loss']).attach(trainer, 'trn_loss')
-    RunningAverage(output_transform=lambda out: out['correct'] / out['total']).attach(trainer, 'trn_accuracy')
+    CustomAccuracyMetric(output_transform=lambda out: out).attach(trainer, 'trn_accuracy')
 
     trainer.run(train_loader, max_epochs=5)
 
