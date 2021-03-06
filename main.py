17ed2c1214c41231ffbb86b463e207947de02be2
Andrew Drozdov <andrew@mrdrozdov.com>
2021-03-06 11:09:49 -0500

    Add model training with example model.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import PIL
 import scipy.io
 from tqdm import tqdm
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
@@ -57,6 +59,33 @@ class CustomDataset(torch.utils.data.Dataset):
         return filenames, labels
 
 
+class Net(nn.Module):
+    def __init__(self, num_classes=10):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 5, 3)
+        self.conv2 = nn.Conv2d(16, 32, 3, 1)
+        self.conv3 = nn.Conv2d(32, 32, 3, 1)
+        self.fc1 = nn.Linear(9248, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = torch.relu(x)
+        x = self.conv2(x)
+        x = torch.relu(x)
+        x = torch.max_pool2d(x, 2)
+        x = self.conv3(x)
+        x = torch.relu(x)
+        x = torch.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = torch.relu(x)
+        x = self.fc2(x)
+        # output = F.log_softmax(x, dim=1)
+        output = x
+        return output
+
+
 def main(args):
     torch.random.manual_seed(args.seed)
 
@@ -73,13 +102,32 @@ def main(args):
         images_folder=train_data_config['images_folder'],
         transform=transform)
     train_loader = torch.utils.data.DataLoader(train_dataset,
-        batch_size=train_data_config.get('batch_size', 16),
+        batch_size=train_data_config.get('batch_size', 128),
         shuffle=True,
         num_workers=train_data_config.get('num_workers', 4))
 
+    model = Net(num_classes=120)
+    opt = optim.Adam(model.parameters(), lr=0.002)
+
     def train_step(engine, batch):
+        model.train()
         images, labels = batch
-        return 0
+        logits = model(images)
+        loss = nn.CrossEntropyLoss()(logits, labels)
+
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+        correct = (logits.max(-1)[1] == labels).sum().item()
+        total = labels.shape[-1]
+
+        out = {}
+        out['loss'] = loss.item()
+        out['correct'] = correct
+        out['total'] = total
+
+        return out
 
     trainer = Engine(train_step)
 
@@ -87,13 +135,13 @@ def main(args):
     def update(engine):
         print('training started!')
 
-    @trainer.on(Events.ITERATION_COMPLETED(every=1))
-    def update(engine):
-        pass
-
     @trainer.on(Events.EPOCH_COMPLETED)
+    @trainer.on(Events.ITERATION_COMPLETED(every=10))
     def log_trn_loss(engine):
-        print('log_trn_loss')
+        log_msg = f"training epoch: {engine.state.epoch}"
+        log_msg += f" | loss: {engine.state.metrics['trn_loss']:.3f}"
+        log_msg += f" | accuracy: {engine.state.metrics['trn_accuracy']:.3f}"
+        print(log_msg)
 
     @trainer.on(Events.EPOCH_COMPLETED)
     def run_dev_eval(engine):
@@ -101,6 +149,9 @@ def main(args):
 
     pbar = ProgressBar()
     pbar.attach(trainer)
+
+    RunningAverage(output_transform=lambda out: out['loss']).attach(trainer, 'trn_loss')
+    RunningAverage(output_transform=lambda out: out['correct'] / out['total']).attach(trainer, 'trn_accuracy')
 
     trainer.run(train_loader, max_epochs=5)
 
